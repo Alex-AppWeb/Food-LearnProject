79b08aadf7e44d454c1603bfb8fc28a7c763cd6e
Alex <kurbatov199@gmail.com>
2022-06-16 10:55:41 +0300

    Добавил функционал перключения табов

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,7 +93,39 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
+  function hideTabConten() {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabsContetn(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabConten();
+  showTabsContetn();
+  tabsParent.addEventListener('click', event => {
+    const target = event.target;
+    if (target && target.classList.contains('tabheader__item')) tabs.forEach((item, i) => {
+      if (target == item) {
+        hideTabConten();
+        showTabsContetn(i);
+      }
+    });
+  });
+});
 
 /***/ })
 
